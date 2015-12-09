87839ae2237298375f326d7a17511c68b7db76a0
nellrcs <nellrcs@gmail.com>
2015-12-09 16:15:51 -0200

    decode link

--- UrlAmigavel.class.php
+++ UrlAmigavel.class.php
@@ -1,6 +1,6 @@
 <?php
  /*
- *  @author    Warllencs.
+ *  @author    Warllen castro dos santos.
  *  @license   http://www.apache.org/licenses/LICENSE-2.0
  */
 class UrlAmigavel
@@ -14,52 +14,117 @@ class UrlAmigavel
 		$barras_amigaveis = explode("/", str_replace(strrchr($_SERVER["PHP_SELF"], "?"), "", $_SERVER["PHP_SELF"]));
 
 		// verifica se depois da ultima '/' existe  algum campo
-		if($total_de_barras[count($total_de_barras) -1] == ''):
+		if($total_de_barras[count($total_de_barras) -1] == '')
+		{
 			//remove o ultimo campo se for vazio
 			unset($total_de_barras[count($total_de_barras) -1]);
-		endif;
+		}
 
 		//retorna a url amigavel como array
 		$retorno = array_slice($total_de_barras, count($barras_amigaveis) - 1);
 
-		if($posicao != null || $posicao === 0):
+		if($posicao != null || $posicao === 0)
+		{
 			//retorna somente a posicao Ex:. campo_url(0) = pagina/ campo_url(1) = id
  			return $retorno[$posicao];
-		else:
+		}
+		else
+		{
 			return $retorno;
-		endif;	
+		}
 	}
 
 	//SE FOR PASSADO O NOME DO CAMPO ELE RETORNA O VALOR DO CAMPO EX: pagina=home / se existir a pagina ele retornara home
-	//SE  NAO FOR PASSADO O NOME DO CAMPO ELE RETORNA UM ARRAY: [0]home [1]produt [2]22 [3]
+	//SE  NAO FOR PASSADO O NOME DO CAMPO ELE RETORNA UM ARAY: [0]home [1]produt [2]22 [3]
 	public static function url_get($campo = null)
 	{
 
-		 if($campo != null):
-		 	if(!empty($_GET[$campo])):
+		 if($campo != null)
+		 {
+		 	if(!empty($_GET[$campo]))
+		 	{
 		 		$retorno = $_GET[$campo];
-		 	else:
+		 	}
+		 	else
+		 	{
 		 		$retorno = false;
-		 	endif;
-		 else:
-		 	if(!empty($_GET)):
-		 		foreach ($_GET as $valor):
+		 	}
+		 }
+		 else
+		 {
+		 	if(!empty($_GET))
+		 	{
+
+		 		foreach ($_GET as $valor)
+		 		{
 		 			$pos = strpos($valor, '/');
-		 			if($pos === false):
+		 			if($pos === false)
+		 			{
 		 				$retorno = array_values($_GET);
-		 			else:
+		 			}
+		 			else
+		 			{
 		 				$retorno = self::campo_url();
 		 				break;
-		 			endif;
-		 		endforeach;
-		 	else:
+		 			}
+		 		}
+		 	}
+		 	else
+		 	{
 		 		$retorno = false;
-		 	endif;
-		 endif;
+		 	}
+		 }
+
 		 return $retorno;
+
 	}
 
-	# pode ser utilizado dentro do BASE	html
+	//VALIDA OS CAMPOS DA URL SE ELES EXSTEM NO INCLUDE DA PAGINA
+	public static function valida_a_url($url_total, $url_master,$numero_de_oks)
+	{
+
+		$numro_da_base = count($url_master);
+
+		$i = array();
+
+		//valida a url gradtivamente se precisar validar ela inteira
+		//cria novo codigo
+		if(count($url_total) < $numero_de_oks)
+		{
+			$numero_de_oks = count($url_total);
+		}
+
+
+		foreach ($url_total as $key => $url_separada)
+		{
+
+			if($key < $numro_da_base)
+			{
+				foreach ($url_master[$key] as $valor)
+				{
+
+					if($url_separada == $valor)
+					{
+						$i[] = $valor;
+					}
+
+				}
+			}
+
+		}
+
+		if(count($i) ==  $numero_de_oks)
+		{
+
+			return $i;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+
 	public static function url_padrao($tipo = 'http')
 	{
 		$barras_amigaveis = explode("/", str_replace(strrchr($_SERVER["PHP_SELF"], "?"), "", $_SERVER["PHP_SELF"]));
@@ -82,18 +147,27 @@ class UrlAmigavel
 		}
 	}
 
-	# concatena id mais o titulo
-	public static function linkAmigavel($id,$titulo)
+	public static function execultar($campos_orden = null,$n_campos_validos = null)
 	{
-		$de = "áàãâéêíóôõúüçÁÀÃÂÉÊÍÓÔÕÚÜÇ";
-		$para = "aaaaeeiooouucAAAAEEIOOOUUC";
-	    $i = array();
-	    $v = array();
-	    preg_match_all('/./u', $de, $i);
-	    preg_match_all('/./u', $para, $v);
-	    $mapa = array_combine($i[0], $v[0]);
-	    return $id."-".strtolower(str_replace(" ", "-", strtr($titulo, $mapa)) );
+		if(self::url_get() == false || !empty($n_campos_validos))
+		{
+			if($campos_orden != null && $n_campos_validos != null)
+			{
+				$retorno  = self::valida_a_url(self::campo_url(), $campos_orden,$n_campos_validos);
+			}
+			else
+			{
+				$retorno  =  self::campo_url();
+			}
+		}
+		else
+		{
+			$retorno  = self::url_get();
+		}
+
+		return $retorno;
 	}
+
 
 	public static function gera_htaccess($dir = null)
 	{
@@ -101,6 +175,7 @@ class UrlAmigavel
 		$arquivo = fopen($dir.".htaccess", "w");
 		$htaccess = "";
 		$htaccess.="RewriteEngine On"."\r\n";
+		$htaccess.="RewriteRule ^sitemap.xml$ sitemap.php [L]"."\r\n";
 		$htaccess.=	"RewriteCond %{SCRIPT_FILENAME} !-f"."\r\n";
 		$htaccess.=	"RewriteCond %{SCRIPT_FILENAME} !-d"."\r\n";
 		$htaccess.=	"RewriteCond $1 !^(index\.php|img|css|js|favicon\.ico|robots\.txt)"."\r\n";
@@ -112,6 +187,24 @@ class UrlAmigavel
 
 	}
 
+	# concatena id mais o titulo (nome)
+	public static function linkAmigavel($id,$titulo)
+	{
+		$de = "áàãâéêíóôõúüçÁÀÃÂÉÊÍÓÔÕÚÜÇ";
+		$para = "aaaaeeiooouucAAAAEEIOOOUUC";
+	    $i = array();
+	    $v = array();
+	    preg_match_all('/./u', $de, $i);
+	    preg_match_all('/./u', $para, $v);
+	    $mapping = array_combine($i[0], $v[0]);
+	    return $id."-".strtolower(str_replace(" ", "-", strtr($titulo, $mapping)) );
+	}
+
+	public static function linkAmigavelDecode($url)
+	{
+		$id = explode("-", $url);
+		return (int)$id[0];
+	}
 }
 
 ?>
